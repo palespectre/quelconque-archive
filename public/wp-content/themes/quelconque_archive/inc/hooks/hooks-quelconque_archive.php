98699edb7488b7094f699c605b216f2ba7c8e5c0
palespectre <cormierpierre1@gmail.com>
2021-01-11 22:35:57 +0100

    Mark product by default as individually sold

--- public/wp-content/themes/quelconque_archive/inc/hooks/hooks-quelconque_archive.php
+++ public/wp-content/themes/quelconque_archive/inc/hooks/hooks-quelconque_archive.php
@@ -1,7 +1,7 @@
 <?php
 
 // stock message
-add_filter('woocommerce_stock_html', 'change_stock_message', 10, 2);
+add_filter('woocommerce_get_stock_html', 'change_stock_message', 10, 2);
 function change_stock_message($message, $stock_status) {
     if ($stock_status == "Out of stock") {
         $message = '<p class="stock out-of-stock">Sold</p>';    
@@ -13,7 +13,7 @@ function change_stock_message($message, $stock_status) {
 add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_stock', 10 );
 function woocommerce_template_loop_stock() {
     global $product;
-    if ($product->stock_status === 'outofstock') {
+    if ($product->get_stock_status() === 'outofstock') {
         echo '<p class="is-out-of-stock">Sold</p>';
     }
 }
@@ -26,3 +26,31 @@ function woocommerce_template_loop_stock() {
 
 // remove related products
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
+// rename product tags' label
+add_filter( 'woocommerce_taxonomy_args_product_tag', 'custom_wc_taxonomy_args_product_tag' );
+function custom_wc_taxonomy_args_product_tag( $args ) {
+	$args['label'] = __( 'Designers', 'woocommerce' );
+	$args['labels'] = array(
+	    'name' 				=> __( 'Designers', 'woocommerce' ),
+	    'singular_name' 	=> __( 'Product Designer', 'woocommerce' ),
+        'menu_name'			=> _x( 'Designers', 'Admin menu name', 'woocommerce' ),
+	    'search_items' 		=> __( 'Search Product Designers', 'woocommerce' ),
+	    'all_items' 		=> __( 'All Product Designers', 'woocommerce' ),
+	    'parent_item' 		=> __( 'Parent Product Designer', 'woocommerce' ),
+	    'parent_item_colon' => __( 'Parent Product Designer:', 'woocommerce' ),
+	    'edit_item' 		=> __( 'Edit Product Designer', 'woocommerce' ),
+	    'update_item' 		=> __( 'Update Product Designer', 'woocommerce' ),
+	    'add_new_item' 		=> __( 'Add New Product Designer', 'woocommerce' ),
+	    'new_item_name' 	=> __( 'New Product Designer Name', 'woocommerce' )
+	);
+
+	return $args;
+}
+
+// check sold individually by default
+add_filter( 'woocommerce_is_sold_individually', 'default_no_quantities', 10, 2 );
+function default_no_quantities( $individually, $product ){
+    $individually = true;
+    return $individually;
+}
