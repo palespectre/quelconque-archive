98bed260e4679a0ba56710250e23c44abc1c3989
palespectre <cormierpierre1@gmail.com>
2021-01-11 22:34:49 +0100

    Update Locomotive-scroll after filters change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,22 @@
 import css from '../css/app.scss';
 import LocomotiveScroll from 'locomotive-scroll';
 
+// init locomotive-scroll
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
 });
 
-
+// update locomotive-scroll once all images are loaded
 let images = document.querySelectorAll('img');
 images.forEach(img => {
     img.onload = function() {
         scroll.update();
-        console.log('update');
     }
+});
+
+// update locomotive-scroll after filters change
+jQuery(document).on('berocket_ajax_products_loaded', function() {
+    scroll.update();
+    console.log('updaaate');
 });
