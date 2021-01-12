197ae87688d70236f8c3085a91e409a3757a8b5e
palespectre <cormierpierre1@gmail.com>
2021-01-13 00:49:29 +0100

    Debug Locomotive-scroll on product page

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,14 +9,34 @@ const scroll = new LocomotiveScroll({
 
 // update locomotive-scroll once all images are loaded
 let images = document.querySelectorAll('img');
-images.forEach(img => {
-    img.onload = function() {
-        scroll.update();
+images.forEach((img, iteration) => {
+    // add condition if images.length > 20, last item
+    if (images.length > 10) {
+        if (iteration === images.length-1) {
+            img.onload = function() {
+                scroll.update();
+            }
+        }
+    } else {
+        img.onload = function() {
+            scroll.update();
+        }
     }
 });
 
 // update locomotive-scroll after filters change
 jQuery(document).on('berocket_ajax_products_loaded', function() {
     scroll.update();
-    console.log('updaaate');
+    scroll.scrollTo("#content", {duration:100});
 });
+
+// prevent wordpress from asking confirmation to navigate away from the current post
+
+// resize product images
+const resizeImage = () => {
+    let picture = document.querySelector('.wp-post-image');
+    picture.width = picture.clientWidth;
+    picture.height = picture.clientHeight;
+}
+
+setTimeout(function(){ scroll.update(); }, 1000);
